9a4b119552127a23748eca19bc482b033bacc579
Neon2905 <tpshine1234@gmail.com>
2025-11-06 12:38:01 +0630

    include images and remove unused CSS; decorated for indentation

--- src/invite/index.js
+++ src/invite/index.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
     const recipient = params.get('name'); // null if not present
 
+    //TODO: Check if there's a valid recipient name
+    
+
     const content = document.getElementById('content');
-    content.innerHTML = `<img src='../resources/${recipient || 'DawNuNuThein'}.jpg' alt='${recipient}'/>`;
+    content.innerHTML = `<img src='../../resources/${recipient || 'default'}.jpg' alt='${recipient}' class="flex md:h-full"/>`;
 });
